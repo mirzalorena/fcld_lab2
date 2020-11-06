ade767313730ac9ba94593bc18eac6da29df58c9
mirzalorena <mlie2568@scs.ubbcluj.ro>
2020-11-06 11:09:24 +0200

    added menu

--- fa/finiteAutomata.py
+++ fa/finiteAutomata.py
@@ -1,3 +1,5 @@
+from scanner import *
+
 class FiniteAutomata:
     def __init__(self, Q, E, S, q0, F):
         self.Q = Q
@@ -12,14 +14,14 @@ class FiniteAutomata:
     def parseLine(line):
         return [value.strip() for value in line.strip().split('=')[1].strip()[1:-1].strip().split(',')]
 
-    def getTransactions(trans):
+    def getTransitions(trans):
         result = []
         transitions = []
-        index = 0
+        count = 0
 
-        while index < len(trans):
-            transitions.append(trans[index] + ',' + trans[index + 1])
-            index += 2
+        while count < len(trans):
+            transitions.append(trans[count] + ',' + trans[count + 1])
+            count += 2
 
         for transition in transitions:
             left, right = transition.split('->')
@@ -36,9 +38,12 @@ class FiniteAutomata:
             E = FiniteAutomata.parseLine(file.readline())
             q0 = file.readline().split('=')[1].strip()
             F = FiniteAutomata.parseLine(file.readline())
-            S = FiniteAutomata.getTransactions(FiniteAutomata.parseLine(''.join([line for line in file])))
+            S = FiniteAutomata.getTransitions(FiniteAutomata.parseLine(''.join([line for line in file])))
 
             return FiniteAutomata(Q, E, S, q0, F)
+
+    def printS(self):
+        return 'S = { ' + '\n '.join([' -> '.join([str(part) for part in trans]) for trans in self.S]) + ' }\n'
 
     def __str__(self):
         return 'Q =' + str(self.Q) + ' \n' \
@@ -47,11 +52,42 @@ class FiniteAutomata:
                + 'q0 = ' + str(self.q0) + '\n'\
                + 'S = { ' + '\n '.join([' -> '.join([str(part) for part in trans]) for trans in self.S]) + ' }\n'
 
+def menu():
+    print("0.Exit")
+    print("1.Set of states")
+    print("2. The alphabet")
+    print("3. Initial state")
+    print("4. Final States")
+    print("5. Transitions")
+    print("6. FA")
 
 if __name__ == '__main__':
     print("Test Finite Automata")
+    menu()
 
-    fa=FiniteAutomata.fromFile("fa.txt")
-    print(fa)
+    scanner = Scanner()
+    option=-1
+
+    fa = FiniteAutomata.fromFile("fa.txt")
+
+    while(option!=0):
+        option=int(input("What do you wanna see?"))
+        if(option==6):
+            print(fa)
+        elif option==1:
+            print(fa.Q)
+        elif option==2:
+            print(fa.E)
+        elif option==3:
+            print(fa.q0)
+        elif option==4:
+            print(fa.F)
+        elif option==5:
+            print(fa.printS())
+
+
+
+
+
 
 
